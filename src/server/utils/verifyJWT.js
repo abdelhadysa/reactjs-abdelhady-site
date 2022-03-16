9ac0539392ca6a2dd65981a8fdaf02bdb1384011
Abdelhady Salah <hadysalah1455@gmail.com>
2022-03-16 03:20:15 +0200

    Sign and verify JWT tokens

---
+++ src/server/utils/verifyJWT.js
@@ -0,0 +1,24 @@
+/*
+    reactjs-abdelhady-site project created and maintained by Abdelhady "H2O" Salah.
+    (c) 2022 Abdelhady Salah <hadysalah1455@gmail.com> (https://github.com/h2o-creator/reactjs-abdelhady-site)
+    All Rights Reserved.
+    Licensed under the GNU GPL v3 License.
+    License file is included in the root directory and has the name "LICENSE"
+*/
+
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+
+dotenv.config()
+
+const verifyJWT = (token = null) => {
+    const jwtSecret = process.env.JWT_SECRET
+    return new Promise((res, rej) => {
+        jwt.verify(token, jwtSecret, (err, decoded) => {
+            if (err) return rej(err)
+            res(decoded)
+        })
+    })
+}
+
+export default verifyJWT
