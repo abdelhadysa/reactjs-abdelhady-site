6eaab4f86cd92ad593b0939a19351d214f94c980
Abdelhady Salah <hadysalah1455@gmail.com>
2022-03-20 22:11:32 +0200

    Make default pass 'seed'

--- src/db/sequelize-seeders/dev/20220315170826-abdelhady.js
+++ src/db/sequelize-seeders/dev/20220315170826-abdelhady.js
@@ -1,11 +1,14 @@
 'use strict';
+import { hashPass } from '../../../server/utils/bcryptManager';
 const { metaUser } = require('../metaSeed')
 module.exports = {
 	async up (queryInterface, Sequelize) {
+		const defaultPass = await hashPass('seed')
 		await queryInterface.bulkInsert('Users', metaUser.map((user) => {
 			return {
 				Uuid: user.Uuid,
 				Username: user.Username,
+				PasswordHash: defaultPass,
 				CreatedAt: new Date(),
 				UpdatedAt: new Date(),
 			}
