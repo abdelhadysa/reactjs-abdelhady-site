21544278a032092cf543546aed48ea6d37f681f2
Abdelhady Salah <hadysalah1455@gmail.com>
2022-03-20 02:30:40 +0200

    Improve permission verification check

--- src/server/middleware/requirePerm.js
+++ src/server/middleware/requirePerm.js
@@ -9,35 +9,31 @@
 const path = require('path')
 
 import models from 'Database/sequelize-models'
-import { Op } from 'sequelize'
 import httpException from '../utils/httpException'
-import isUuid from '../utils/isUuid'
 
 const { User, Permission } = models
 
-const requirePerm = async (req, res, next, permissionName) => {
+const requirePerm = async (req, res, next) => {
     if (!req.jwtToken || !req.decodedJWTPayload) return next(new httpException(401, 'Missing payload data'))
-    const { decodedJWTPayload } = req
+    const { decodedJWTPayload, permNeeded } = req
     try {
         const permission = await Permission.scope('hideSensitive').findOne({
             where: {
-                [Op.or]: [ { Name: permissionName }, { Name: 'Full access' } ],
+                Name: permNeeded,
             },
         })
         if (!permission) return next(new httpException(404, 'Permission name not found'))
         const user = await User.scope('hideSensitive').findOne({
             where: {
-                Username: {
-                    [Op.eq]: decodedJWTPayload.username,
-                },    
+                Username: decodedJWTPayload.username,    
             },
         })
+        if (!user) return next(new httpException(404, 'User name not found'))
         const userRoles = await user.getRoles()
-        userRoles.forEach(async (role, index) => {
-            const roleHasPermission = await role.hasPermissions(permission)
-            if (roleHasPermission === true) return next()
-            else if (index === (userRoles.length - 1)) return next(new httpException(401, 'Unauthorized access'))
-        })
+        for (const role of userRoles) {
+            if (await role.hasPermission(permission)) return next()
+        }
+        return next(new httpException(401, 'Unauthorized access'))
     } catch (e) {
         return next(new httpException(500, e))
     }
