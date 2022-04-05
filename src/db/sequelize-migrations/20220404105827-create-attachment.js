cdd7e41c7d43de1ad29e0fd8be9a581627e7d781
Abdelhady Salah <hadysalah1455@gmail.com>
2022-04-05 14:10:44 +0200

    Create attachments migration

---
+++ src/db/sequelize-migrations/20220404105827-create-attachment.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Attachments', {
+			Uuid: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				primaryKey: true,
+				allowNull: false,
+			},
+			Name: {
+				type: Sequelize.STRING
+			},
+			Description: {
+				type: Sequelize.STRING
+			},
+			AttachmentUrl: {
+				type: Sequelize.STRING
+			},
+			UserUuid: {
+				type: Sequelize.UUID,
+				allowNull: false,
+				references: {
+					model: 'Users',
+					key: 'Uuid',
+					as: 'UserUuid',
+				},
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			},
+			MessageUuid: {
+				type: Sequelize.UUID,
+				allowNull: false,
+				references: {
+					model: 'Messages',
+					key: 'Uuid',
+					as: 'MessageUuid',
+				},
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			},
+			CreatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			UpdatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Attachments');
+	}
+};
