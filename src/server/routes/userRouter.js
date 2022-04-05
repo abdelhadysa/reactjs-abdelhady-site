4343a1c2f39fb0d5128df3ddc52a4012a14cf43d
Abdelhady Salah <hadysalah1455@gmail.com>
2022-04-05 14:18:53 +0200

    Add file uploads

--- src/server/routes/userRouter.js
+++ src/server/routes/userRouter.js
@@ -10,18 +10,26 @@ const path = require('path')
 const express = require('express')
 import * as userController from '../controllers/userController'
 import requirePerm from '../middleware/requirePerm'
-import { D_USER_PERM } from '../utils/defaults'
+import { D_USER_PERM, D_DEFAULT_AVATAR_PATH, D_DEFAULT_ATTACHMENTS_PATH, D_DEFAULT_MAX_ATTACHMENTS } from '../utils/defaults'
 import multer from 'multer'
 
-const storage = multer.diskStorage({
-    destination: path.resolve('dist/', 'uploads'),
+const avatarStorage = multer.diskStorage({
+    destination: path.resolve(D_DEFAULT_AVATAR_PATH),
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.')[1])
+    }
+})
+
+const attachmentStorage = multer.diskStorage({
+    destination: path.resolve(D_DEFAULT_ATTACHMENTS_PATH),
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.')[1])
     }
 })
   
-const upload = multer({ storage: storage, fileFilter: function fileFilter (req, file, cb) {
+const uploadAvatar = multer({ storage: avatarStorage, fileFilter: function fileFilter (req, file, cb) {
     if (file.size > 1000000) return cb(null, false)
     const ext = file.originalname.split('.')[1]
     switch (ext) {
@@ -32,22 +40,38 @@ const upload = multer({ storage: storage, fileFilter: function fileFilter (req, 
     }
 } })
 
+const uploadAttachments = multer({ storage: attachmentStorage, fileFilter: function fileFilter (req, file, cb) {
+    if (file.size > 8000000) return cb(null, false)
+    const ext = file.originalname.split('.')[1]
+    switch (ext) {
+        case 'jpg':
+        case 'jpeg':
+        case 'png':
+        case 'gif':
+        case 'pdf':
+        case 'txt':
+        case 'md': return cb(null, true)
+        default: return cb(null, false)
+    }
+} })
+
 const userRouter = express.Router()
 
 userRouter.get('/', [requirePerm(D_USER_PERM.GET_ALL), userController.getAll])
 userRouter.get('/:id', [requirePerm(D_USER_PERM.GET), userController.getOne])
 userRouter.post('/', [requirePerm(D_USER_PERM.CREATE), userController.createOne])
-userRouter.put('/:id', [requirePerm(D_USER_PERM.UPDATE), upload.single('avatar'), userController.updateOne])
+userRouter.put('/:id', [requirePerm(D_USER_PERM.UPDATE), uploadAvatar.single('avatar'), userController.updateOne])
 userRouter.delete('/:id', [requirePerm(D_USER_PERM.DELETE), userController.deleteOne])
 
 userRouter.get('/:id/post', [requirePerm(D_USER_PERM.GET_POSTS), userController.getPosts])
-userRouter.post('/:id/post', [requirePerm(D_USER_PERM.CREATE_POST), userController.createPost])
-userRouter.put('/:id/post/:postId', [requirePerm(D_USER_PERM.UPDATE_POST), userController.updatePost])
+userRouter.post('/:id/post', [requirePerm(D_USER_PERM.CREATE_POST), uploadAttachments.array('attachments', D_DEFAULT_MAX_ATTACHMENTS), userController.createPost])
+userRouter.put('/:id/post/:postId', [requirePerm(D_USER_PERM.UPDATE_POST), uploadAttachments.array('attachments', D_DEFAULT_MAX_ATTACHMENTS), userController.updatePost])
 userRouter.delete('/:id/post/:postId', [requirePerm(D_USER_PERM.DELETE_POST), userController.deletePost])
 
 userRouter.get('/:id/reply', [requirePerm(D_USER_PERM.GET_REPLIES), userController.getReplies])
-userRouter.post('/:id/reply/:postId', [requirePerm(D_USER_PERM.CREATE_REPLY), userController.createReply])
-userRouter.put('/:id/reply/:replyId', [requirePerm(D_USER_PERM.UPDATE_REPLY), userController.updateReply])
+userRouter.post('/:id/reply/:postId', [requirePerm(D_USER_PERM.CREATE_REPLY), uploadAttachments.array('attachments', D_DEFAULT_MAX_ATTACHMENTS), userController.createReply])
+userRouter.post('/:id/reply/:postId/:replyId', [requirePerm(D_USER_PERM.CREATE_REPLY), uploadAttachments.array('attachments', D_DEFAULT_MAX_ATTACHMENTS), userController.createReply])
+userRouter.put('/:id/reply/:replyId', [requirePerm(D_USER_PERM.UPDATE_REPLY), uploadAttachments.array('attachments', D_DEFAULT_MAX_ATTACHMENTS), userController.updateReply])
 userRouter.delete('/:id/reply/:replyId', [requirePerm(D_USER_PERM.DELETE_REPLY), userController.deleteReply])
 
 userRouter.get('/:id/role', [requirePerm(D_USER_PERM.GET_ROLES), userController.getRoles])
