f35fd76b3c70212934fe944f02dcf00fdef4f61f
Abdelhady Salah <hadysalah1455@gmail.com>
2022-04-10 01:16:04 +0200

    Fix indentation

--- src/db/sequelize-config/config.js
+++ src/db/sequelize-config/config.js
@@ -2,28 +2,28 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-  "development": {
-    "username": process.env.DEV_DB_USER,
-    "password": process.env.DEV_DB_PASS,
-    "database": process.env.DEV_DB_NAME,
-    "host": process.env.DEV_DB_HOST,
-    "dialect": "postgres",
-    "logging": (process.env.NODE_ENV === 'development' ? console.log : false)
-  },
-  "test": {
-    "username": process.env.TEST_DB_USER,
-    "password": process.env.TEST_DB_PASS,
-    "database": process.env.TEST_DB_NAME,
-    "host": process.env.TEST_DB_HOST,
-    "dialect": "postgres",
-    "logging": (process.env.NODE_ENV === 'development' ? console.log : false)
-  },
-  "production": {
-    "username": process.env.PROD_DB_USER,
-    "password": process.env.PROD_DB_PASS,
-    "database": process.env.PROD_DB_NAME,
-    "host": process.env.PROD_DB_HOST,
-    "dialect": "postgres",
-    "logging": (process.env.NODE_ENV === 'development' ? console.log : false)
-  }
+	"development": {
+		"username": process.env.DEV_DB_USER,
+		"password": process.env.DEV_DB_PASS,
+		"database": process.env.DEV_DB_NAME,
+		"host": process.env.DEV_DB_HOST,
+		"dialect": "postgres",
+		"logging": (process.env.NODE_ENV === 'development' ? console.log : false)
+	},
+	"test": {
+		"username": process.env.TEST_DB_USER,
+		"password": process.env.TEST_DB_PASS,
+		"database": process.env.TEST_DB_NAME,
+		"host": process.env.TEST_DB_HOST,
+		"dialect": "postgres",
+		"logging": (process.env.NODE_ENV === 'development' ? console.log : false)
+	},
+	"production": {
+		"username": process.env.PROD_DB_USER,
+		"password": process.env.PROD_DB_PASS,
+		"database": process.env.PROD_DB_NAME,
+		"host": process.env.PROD_DB_HOST,
+		"dialect": "postgres",
+		"logging": (process.env.NODE_ENV === 'development' ? console.log : false)
+	}
 }
